94063220a0951e48d8cd38614249293f84d2f551
Arpanradadiya12 <arpanpatel.radadiya@gmail.com>
2025-12-27 09:46:25 +0530

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    supabaseUrl: 'https://hfevhneunlqvsqonhkrx.supabase.co',
+    supabaseKey: 'sb_publishable_hKtHAQyH2HSA-UjtJ2Z5cg_lc_2Yf0a',
+};
